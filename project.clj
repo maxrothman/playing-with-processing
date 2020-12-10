f5216c74bb000da6363f8575cdfc1c9be2ef957e
Max Rothman <max.r.rothman@gmail.com>
2020-12-10 09:30:13 -0500

    Still working on native image

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.2-alpha4"]
                  [quil "3.1.0"]]
   :plugins [[io.taylorwood/lein-native-image "0.3.1"]]
   :native-image {:name "ice-melting-sim"
                  :graal-bin "graalvm-ce-java8-20.3.0/Contents/Home/"
-                 :opts ["--no-server" "--report-unsupported-elements-at-runtime" "--no-fallback"]}
+                 :opts ["--no-server"
+                        "--no-fallback"]}
   :profiles {:uberjar {:aot :all
                        :native-image {:jvm-opts ["-Dclojure.compiler.direct-linking=true"]}}}
   :main "ice-melting-sim.core"
